cd1b3bfe768d812746694e08b8d72bc12e97d99b
seannse <seannse@gmail.com>
2023-01-18 22:49:02 +0200

    asf

--- src/js/index.js
+++ src/js/index.js
@@ -86,6 +86,7 @@ function onPixabayApiResolvedMore(obj) {
 
   if (!hits.length) {
     Notify.info("We're sorry, but you've reached the end of search results.");
+    observer.unobserve(refs.target);
   };
 }
 
